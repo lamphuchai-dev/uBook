2e57ad0a62e6f08ec9688ee38d5756108276a4ee
LamPhucHai <lamphuchai.dev@gmail.com>
2023-11-04 22:24:13 +0700

    up

--- ext-book/extensions/anime_hay/src/chapter.js
+++ ext-book/extensions/anime_hay/src/chapter.js
@@ -1,21 +1,27 @@
 async function chapter(url, page) {
   var res = await Extension.request(url);
+  var result = [];
   // var data = res.match(/https:\/\/suckplayer\.xyz\/video\/[a-zA-Z0-9_-]+/g);
-  var data = res.match(/https:\/\/\playhydrax.com\/\?v=[^&"\s]+/g);
-
-  if (!data) {
-    var data = res.match(/source_fbo: \[(.*?)\]/);
-    var urlVideo = extractAllText(data[1])[1];
-    if (!urlVideo) return null;
-    return [urlVideo];
-  }
-  return [
-    {
+  var link1 = res.match(/https:\/\/\playhydrax.com\/\?v=[^&"\s]+/g);
+  if (link1) {
+    result.push({
       url: data[0],
       type: "iframe",
       regex: "playhydrax|player",
-    },
-  ];
+    });
+  }
+  var link2 = res.match(/source_fbo: \[(.*?)\]/);
+
+  if (link2) {
+    var urlVideo = extractAllText(data[1])[1];
+    if (urlVideo) {
+      result.push({
+        url: urlVideo,
+        type: "video",
+      });
+    }
+  }
+  return result;
 }
 
 function extractAllText(str) {
