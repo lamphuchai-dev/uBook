fe28ae4ad606db11f1eea8afc3325ed07c018dea
LamPhucHai <lamphuchai.dev@gmail.com>
2023-11-04 22:35:49 +0700

    up

--- ext-book/extensions/anime_hay/src/chapter.js
+++ ext-book/extensions/anime_hay/src/chapter.js
@@ -1,7 +1,9 @@
-async function chapter(url, page) {
+async function chapter(url) {
   var res = await Extension.request(url);
-  var result = [];
+
   // var data = res.match(/https:\/\/suckplayer\.xyz\/video\/[a-zA-Z0-9_-]+/g);
+
+  var result = [];
   var link1 = res.match(/https:\/\/\playhydrax.com\/\?v=[^&"\s]+/g);
   if (link1) {
     result.push({
@@ -10,26 +12,20 @@ async function chapter(url, page) {
       regex: "playhydrax|player",
     });
   }
-  var matchs = res.match(/source_fbo: \[(.*?)\]/);
 
-  if (matchs) {
-    var link2 = extractAllText(matchs[1])[1];
-    if (link2) {
-      result.push({
-        url: link2,
-        type: "video",
-      });
-    }
+  var link2 = res.match(/\"https:\/\/scontent\.cdninstagram\.com\/[^\s]+\"/g);
+
+  if (link2) {
+    result.push({
+      url: link2[0],
+      type: "video",
+    });
   }
   return result;
 }
 
-function extractAllText(str) {
-  var re = /"(.*?)"/g;
-  var result = [];
-  var current;
-  while ((current = re.exec(str))) {
-    result.push(current.pop());
-  }
-  return result;
-}
+runFn(() =>
+  chapter(
+    "https://animehay.city/xem-phim/tran-hon-nhai-phan-3-tap-9-58893.html"
+  )
+);
