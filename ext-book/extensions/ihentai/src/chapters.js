24e116418d91ca894aa0051f8da4565d05cd18c1
LamPhucHai <lamphuchai.dev@gmail.com>
2023-11-07 10:59:17 +0700

    up

--- ext-book/extensions/ihentai/src/chapters.js
+++ ext-book/extensions/ihentai/src/chapters.js
@@ -1,6 +1,5 @@
 async function chapters(bookUrl) {
   const res = await Extension.request(bookUrl);
-  const host = "https://ihentai.de";
 
   var result = [];
 
@@ -14,24 +13,5 @@ async function chapters(bookUrl) {
     index: 0,
   });
 
-  const tmp = await Extension.querySelector(
-    res,
-    'div[class ="tw-col-span-3 lg:tw-col-span-1"] div.tw-mb-5'
-  ).outerHTML;
-
-  var el = await Extension.querySelector(
-    tmp,
-    'h2[class = "tw-mb-1 tw-font-medium"] span'
-  ).text;
-  if (el.trim() == "Phim gợi ý") {
-    result.push({
-      title: await Extension.querySelector(tmp, "h3 a").text,
-      url: host + (await Extension.getAttributeText(tmp, "h3 a", "href").text),
-      bookUrl,
-      index: 1,
-    });
-  }
-
-  // 
   return result;
 }
